b7598a94197951e91e0019eb96f5eb41615e39f3
devhoodit <hoodit@naver.com>
2023-12-18 17:14:16 +0900

    progress: TData

--- tnn/tensor.py
+++ tnn/tensor.py
@@ -2,6 +2,119 @@ from __future__ import annotations
 from typing import List
 import math
 
+# from enum import Enum, auto
+
+
+# class DType(Enum):
+#     Int = auto()
+#     Float = auto()
+
+# class TData():
+#     def __init__(self, data, dtype=None) -> None:
+#         self._data: List = []
+#         if isinstance(data, (float, int)):
+#             self._data.append(data)
+#             self.shape = tuple()
+#             if dtype == None:
+#                 if isinstance(data, float):
+#                     dtype = DType.Float
+#                 elif isinstance(data, int):
+#                     dtype = DType.Int
+#             self.dtype = dtype
+#             return
+#         elif isinstance(data, list):
+#             for d in data:
+#                 self._data.append(TData(d))
+#             self.shape = (len(data),) + tuple(*self._data[-1].shape)
+#             self.dtype = self._data[-1].dtype
+#             return
+#         elif isinstance(data, TData):
+#             self._data = data._data
+#             self.shape = data.shape
+#             self.dtype = data.dtype
+#             return
+#         raise ValueError()
+    
+#     @property
+#     def data(self) -> float | int | List[TData]:
+#         if self.shape == tuple():
+#             return self._data[-1]
+#         return self._data
+
+#     def __add__(self, other: TData):
+#         if self.dtype == other.dtype:
+#             if self.shape != tuple():
+#                 # array
+#                 if self.shape != other.shape:
+#                     raise ValueError()
+#                 return TData([d1 + d2 for d1, d2 in zip(self._data, other._data)], dtype=self.dtype)
+#             return TData(self._data[0] + other._data[0])
+#         if self.shape == tuple():
+#             # single + array
+#             return TData([self.data + d1 for d1 in other._data], )
+#         else:
+#             # array + single
+#             return TData([d1 + self.data for d1 in self._data])
+#         raise ValueError()
+    
+#     def __sub__(self, other: TData):
+#         if self.dtype == other.dtype:
+#             if self.shape != tuple():
+#                 # array
+#                 if self.shape != other.shape:
+#                     raise ValueError()
+#                 return TData([d1 - d2 for d1, d2 in zip(self._data, other._data)], dtype=self.dtype)
+#             return TData(self._data[0] + other._data[0])
+#         if self.shape == tuple():
+#             # single + array
+#             return TData([self.data - d1 for d1 in other._data], )
+#         else:
+#             # array + single
+#             return TData([d1 - self.data for d1 in self._data])
+#         raise ValueError()
+    
+#     def __mul__(self, other: TData):
+#         if self.shape == other.shape:
+#             if self.shape != tuple():
+#                 # array, array
+#                 if self.shape != other.shape:
+#                     raise ValueError()
+#                 return TData([d1 * d2 for d1, d2 in zip(self._data, other._data)], dtype=self.dtype)
+#             # single, single
+#             return TData(self._data[0] * other._data[0])
+#         if self.shape == tuple():
+#             # single + array
+#             return TData([self.data * d1 for d1 in other._data], )
+#         else:
+#             return TData([d1 * other._data[0] for d1 in self._data])
+#         raise ValueError()
+    
+#     def __pow__(self, other: TData):
+#         if self.dtype == other.dtype:
+#             if self.shape != tuple():
+#                 # array
+#                 if self.shape != other.shape:
+#                     raise ValueError()
+#                 return TData([d1 ** d2 for d1, d2 in zip(self._data, other._data)], dtype=self.dtype)
+#             return TData(self._data + other._data)
+#         if self.shape == tuple():
+#             # single + array
+#             return TData([self.data ** d1 for d1 in other._data], )
+#         else:
+#             # array + single
+#             return TData([d1 ** self.data for d1 in self._data])
+#         raise ValueError()
+    
+# #     def __str__(self) -> str:
+# #         return f"""data: {self._data}
+# # shape: {self.shape}
+# # type: {self.dtype}"""
+    
+#     def __repr__(self) -> str:
+#         if self.shape == tuple():
+#             return f"{self._data[0]}"
+#         return f"{self._data}"
+
 class Tensor():
     def __init__(self, data: float, require_grad=False) -> None:
         self.data = data
